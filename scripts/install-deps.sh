04016ed303f7787797e24acc2fed93c6224a73a2
Félix Hamel <fhamel@optimaweb.ca>
2018-12-19 19:55:37 -0500

    Update install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -33,17 +33,7 @@ wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/downloa
 
 export GNUPGHOME="$(mktemp -d)"
 
-for server in 
-	ha.pool.sks-keyservers.net \
-	hkp://p80.pool.sks-keyservers.net:80 \
-	keyserver.ubuntu.com \
-	hkp://keyserver.ubuntu.com:80 \
-	pgp.mit.edu \
-; do
-	if gpg --keyserver "$server" --recv-keys "B42F6819007F00F88E364FD4036A9C25BF357DD4"; then
-		exit 0
-	fi
-done
+gpg --keyserver keyserver.ubuntu.com --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
 gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu
 
 rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc
